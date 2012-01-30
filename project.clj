3290460a0dbd24ebb3d67644c9ffe94d67f3b679
Andrew Kirilenko <andrew.kirilenko@gmail.com>
2012-01-30 13:56:11 +0300

    Initial implementation

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject iron_mq_clojure "1.0.0"
+  :description "IronMQ Clojure Client"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [cheshire "2.0.6"]]
+  :multi-deps {"1.2.1" [[org.clojure/clojure "1.2.1"]
+                        [cheshire "2.0.6"]]})
