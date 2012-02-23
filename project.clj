700c42bde8a88674a22f4a4ee91336ee30063dee
Andrew Kirilenko <andrew.kirilenko@gmail.com>
2012-02-23 14:16:15 +0300

    Version bump.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject iron_mq_clojure "1.0.1"
+(defproject iron_mq_clojure "1.0.2"
   :description "IronMQ Clojure Client"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [cheshire "2.0.6"]]
+                 [cheshire "2.2.0"]]
   :multi-deps {"1.2.1" [[org.clojure/clojure "1.2.1"]
-                        [cheshire "2.0.6"]]})
+                        [cheshire "2.2.0"]]})
